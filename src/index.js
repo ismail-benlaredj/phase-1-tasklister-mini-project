166671f25827bc6c0157bff3a37bb29f7fe8593e
ismail <yako_is@yahoo.fr>
2023-08-15 11:09:46 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const todoInput = document.getElementById('new-task-description')
+  const todoList = document.getElementById("tasks")
+  const form = document.getElementById("create-task-form")
+
+  const addTodo = (e) => {
+    e.preventDefault()
+    const todo = createTodoItem(todoInput.value)
+    todoList.appendChild(todo)
+  }
+
+  const createTodoItem = (todo) => {
+    const item = document.createElement("li")
+    const btn = document.createElement("button")
+    btn.innerText = " X "
+    btn.style.marginLeft = '5px'
+    btn.addEventListener('click', () => item.remove())
+    item.innerText = todo
+    item.appendChild(btn)
+    return item
+  }
+  form.addEventListener('submit', (e) => addTodo(e))
 });
